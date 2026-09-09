f91699e2b44a0c43b116ec64153d143c760e34be
costifirri <costi.firri@gmail.com>
2026-09-09 12:54:14 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker minimale: cache-first per gli asset, network-first per il documento.
-const CACHE = 'ora-v31'
+const CACHE = 'ora-v32'
 
 self.addEventListener('install', e => {
   self.skipWaiting()
